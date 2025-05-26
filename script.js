6f1be962a4e804fae0c50784f0e280a00a3a5a79
Ali Aizad <aliaizad72@gmail.com>
2025-05-26 10:32:03 +0800

    Change UI (#3)
    * Make it easier to read
    
    * Edit a bit more
    
    * Add additional styling
    
    * Change border
    
    * Add publisher form
    
    * Add publisher name to each accordion
    
    * Add button to query for articles by publisher
    
    * Add button to pull from bq for publisher
    
    * Seperate pull and fetching forms for publishers
    
    * Add limit to pull articles by publisher
    
    * Change baseUrl

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const articlesContainer = document.getElementById("articles-container")
 const articlesHeader = document.getElementById("articles-header")
 const errorHeader = document.getElementById("error")
 const index = document.getElementById("index")
+const publisherArticlesBtn = document.getElementById("publisher-query-submit")
+const publisherPullBtn = document.getElementById("publisher-pull-submit")
 const baseUrl = "https://inbrain-97862438951.asia-southeast1.run.app";
 // const baseUrl = "http://127.0.0.1:3000";
 
@@ -17,15 +19,86 @@ getArticleBtn.addEventListener("click", getArticle);
 regenerateBtn.addEventListener("click", regenerateArticles)
 index.addEventListener("click", (e) => {
   e.preventDefault()
-  allArticles()
-})
-
-
-async function allArticles () {
   const limit = document.getElementById("limit").value
   const langInput = document.getElementById("language")
   const languageCode = langInput.value
-  const params = new URLSearchParams({ limit: limit, language_code: languageCode }).toString()
+  const language = langInput.options[langInput.selectedIndex].textContent
+  const headerTextContent = `${limit} Most Recent Articles in ${language} Language`
+  const params = {languageCode: languageCode, limit: limit, headerTextContent: headerTextContent}
+  allArticles(params)
+})
+
+publisherArticlesBtn.addEventListener("click", (e) => {
+  e.preventDefault()
+  const limit = document.getElementById("publisher-query-limit").value
+  const newsPublisherInput = document.getElementById("publisher-list-2")
+  const newsPublisherId = newsPublisherInput.value
+  const newsPublisherName = newsPublisherInput.options[newsPublisherInput.selectedIndex].textContent
+  const headerTextContent = `${limit} Most Recent Articles for ${newsPublisherName}`
+  const params = {newsPublisherId: newsPublisherId, limit: limit, headerTextContent: headerTextContent}
+  allArticles(params)
+})
+
+publisherPullBtn.addEventListener("click", (e) => {
+  e.preventDefault()
+  const limit = document.getElementById("publisher-pull-limit").value
+  const newsPublisherInput = document.getElementById("publisher-list-1")
+  const newsPublisherId = newsPublisherInput.value
+  const newsPublisherName = newsPublisherInput.options[newsPublisherInput.selectedIndex].textContent
+  const headerTextContent = `${limit} Most Recent Articles for ${newsPublisherName}`
+  pullLatestArticlesForPublisher(newsPublisherId, limit, headerTextContent)
+})
+
+async function pullLatestArticlesForPublisher(newsPublisherId, limit, headerTextContent) {
+  const payload = JSON.stringify({ news_publisher_id: newsPublisherId, limit: limit })
+  const response = await fetch(`${baseUrl}/v1/news_publishers/pull`, {
+    method: "POST",
+    body: payload,
+    headers: {
+      Authorization: `Bearer ${api_key}`,
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+    credentials: "include"
+  });
+  const data = await response.json()
+  console.log(data)
+  clearAccordions()
+  setArticleHeader(headerTextContent)
+  data.forEach((article, i) => {
+    createAccordion(i, article)
+  })
+  window.initFlowbite()
+}
+
+
+async function getNewsPublishersList() {
+  const response = await fetch(`${baseUrl}/v1/news_publishers`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${api_key}`,
+    },
+    credentials: "include"
+  });
+
+  const data = await response.json()
+  const publisherLists = document.getElementsByClassName("publisher-list")
+  data.forEach((publisher, i) => {
+    [...publisherLists].forEach((select) => {
+      const option = document.createElement("option")
+      option.value = publisher.id
+      option.textContent = publisher.name
+
+      if (i === 0) {
+        option.selected = true
+      }
+
+      select.appendChild(option)
+    })
+  })
+}
+
+async function allArticles ({languageCode = "", newsPublisherId = "", limit = "10", headerTextContent = "" }) {
+  const params = new URLSearchParams({ limit: limit, language_code: languageCode, news_publisher_id: newsPublisherId }).toString()
   const response = await fetch(`${baseUrl}/v1/articles/?${params}` , {
     method: "GET",
     headers: {
@@ -34,9 +107,9 @@ async function allArticles () {
     credentials: "include"
   });
   const data = await response.json()
+  console.log(data)
   clearAccordions()
-  const language = langInput.options[langInput.selectedIndex].textContent
-  setArticleHeader(`${limit} Most Recent Articles in ${language} Language`)
+  setArticleHeader(headerTextContent)
   data.forEach((article, i) => {
     createAccordion(i, article)
   })
@@ -170,7 +243,9 @@ window.onload = () => {
   if (api_key === null) {
     location.href = "index.html"
   } else {
-    allArticles()
+    getNewsPublishersList()
+    const headerTextContent = "10 Most Recent Articles in All Language"
+    allArticles({ headerTextContent: headerTextContent })
   }
 };
 
